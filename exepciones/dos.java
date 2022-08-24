70449472b345a25091c2f73cc5794572da6eaac2
matias cornejo <maticornejo99@gmail.com>
2022-08-24 20:04:30 -0300

    Trabajamos sobre el tp 1 de excepciones, quedamos en el punto 8

---
+++ exepciones/dos.java
@@ -0,0 +1,31 @@
+public class dos {
+    private static int metodo() {
+        int valor = 0;
+        try {
+            valor = valor + 1;
+            valor = valor + Integer.parseInt("W");
+            valor = valor + 1;
+            System.out.println("Valor al final del try: " + valor);
+        } catch (NumberFormatException e) {
+            // TODO: handle exception
+            valor = valor + Integer.parseInt("42");
+            System.out.println("Valor al final del catch: " + valor);
+        } finally {
+            valor = valor + 1;
+            System.out.println("Valor al final del finally: " + valor);
+        }
+        valor = valor + 1;
+        System.out.println("Valor antes del return: " + valor);
+        return valor;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(metodo());
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println("Excepcion en metodo()");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ exepciones/pruebaExcep.java
@@ -0,0 +1,17 @@
+class ExepcionEdad extends Exception{
+    /*Crear la clase, constructor con super de Exception, y generar error*/
+}
+
+public class pruebaExcep {
+    /*metodo que recibe la edad de una persona y dispara una exepcion si
+    la persona es menor de edad*/
+    public void menor_deEdad(int edad) { 
+        try {
+            
+        } catch (Exception e) {
+            
+        }    
+    }
+
+}
+

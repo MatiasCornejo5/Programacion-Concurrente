e2bb0dc07cbe8e4e5559ed1f98cef6b3c2679e8c
Matias Cornejo <maticornejo99@gmail.com>
2022-08-26 00:41:36 -0300

    Agregue el inciso b del punto 8 (ruleta)

--- exepciones/pruebaExcep.java
+++ exepciones/pruebaExcep.java
@@ -1,11 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 class ExcepcionEdad extends Exception {
-    /* Crear la clase, constructores uno sin argumentos y otro con argumentos, y generar error */
-    public ExcepcionEdad(){     
+    /*
+     * Crear la clase, constructores uno sin argumentos y otro con argumentos, y
+     * generar error
+     */
+    public ExcepcionEdad() {
     }
+
     public ExcepcionEdad(String arg) {
         super(arg);
     }
 
+}
+
+class ExcepcionRuleta extends Exception {
+    public ExcepcionRuleta(){        
+    }
+    public ExcepcionRuleta(String arg){
+        super(arg);
+    }
 }
 
 public class pruebaExcep {
@@ -17,11 +32,10 @@ public class pruebaExcep {
         try {
             verificarEdad(edad);
         } catch (ExcepcionEdad e) {
-            //System.out.println(e.getMessage());
+            // System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
-
     public static void verificarEdad(int edad) throws ExcepcionEdad {
         if (edad >= 18) {
             System.out.println("Es mayor de edad");
@@ -30,8 +44,41 @@ public class pruebaExcep {
         }
     }
 
-    public static void main (String [] args) {
-        menor_deEdad(16);
+    /*
+     * metodo que recibe un numero de la ruleta y dispara una excepcion si
+     * no sale dicho numero
+     */
+    public static void ruleta() {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        try {
+            System.out.println("Ingrese un numero: ");
+            numero = sc.nextInt();
+            Random aleatorio= new Random();
+            int n=aleatorio.nextInt(36);
+            verificarNumero(numero,n);
+        } catch (ExcepcionRuleta e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void verificarNumero(int numero, int aleatorio) throws ExcepcionRuleta{
+        if (numero==aleatorio) {
+            System.out.println("FELICITACIONES HA GANADO");
+        } else {
+            throw new ExcepcionRuleta("El numero ganador es: <"+aleatorio+">.Lo siento, su numero no ha salido");           
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*menor_deEdad(16);*/
+        String respuesta="si";
+        while (respuesta.equals("si")) {
+            ruleta();
+            System.out.println("Desea seguir jugando?");
+            respuesta=sc.nextLine();
+        }
     }
 
 }
